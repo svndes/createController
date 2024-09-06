91bce25f3b85c81b147c815776e29d26045ec2df
Sandesh Chakradhar <svndes@gmail.com>
2024-09-06 20:03:57 +0545

    update in naming convention

--- createController.py
+++ createController.py
@@ -8,6 +8,7 @@ from PySide2 import QtWidgets, QtCore, QtGui
 spath = os.path.dirname(__file__)
 spath = spath.replace("\\", "/")
 
+
 class CreateControllerUI(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(CreateControllerUI, self).__init__(parent)
@@ -15,90 +16,90 @@ class CreateControllerUI(QtWidgets.QDialog):
         self.setFixedSize(480, 350)
         self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
 
-        main_layout = QtWidgets.QVBoxLayout(self)
-        self.create_radio = QtWidgets.QRadioButton("Create")
-        self.change_shape_radio = QtWidgets.QRadioButton("Change Shape")
-        self.create_radio.setChecked(True)
-        self.range_combo = QtWidgets.QComboBox()
-        self.range_combo.addItems(["Only Ctrl", "Constraint", "Joint Based", "Connection"])
-        self.range_combo.setFixedWidth(188)
+        mainLayout = QtWidgets.QVBoxLayout(self)
+        self.createRadio = QtWidgets.QRadioButton("Create")
+        self.changeShapeRadio = QtWidgets.QRadioButton("Change Shape")
+        self.createRadio.setChecked(True)
+        self.rangeCombo = QtWidgets.QComboBox()
+        self.rangeCombo.addItems(["Only Ctrl", "Constraint", "Joint Based", "Connection"])
+        self.rangeCombo.setFixedWidth(188)
 
-        radio_combo_layout = QtWidgets.QHBoxLayout()
-        radio_combo_layout.addWidget(self.create_radio)
-        radio_combo_layout.addWidget(self.range_combo)
-        radio_layout = QtWidgets.QVBoxLayout()
-        radio_layout.addLayout(radio_combo_layout)
-        radio_layout.addWidget(self.change_shape_radio)
-        main_layout.addLayout(radio_layout)
+        radioComboLayout = QtWidgets.QHBoxLayout()
+        radioComboLayout.addWidget(self.createRadio)
+        radioComboLayout.addWidget(self.rangeCombo)
+        radioLayout = QtWidgets.QVBoxLayout()
+        radioLayout.addLayout(radioComboLayout)
+        radioLayout.addWidget(self.changeShapeRadio)
+        mainLayout.addLayout(radioLayout)
 
-        scroll_area = QtWidgets.QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        main_layout.addWidget(scroll_area)
-        self.scroll_widget = QtWidgets.QWidget()
-        scroll_area.setWidget(self.scroll_widget)
-        self.grid_layout = QtWidgets.QGridLayout(self.scroll_widget)
+        scrollArea = QtWidgets.QScrollArea()
+        scrollArea.setWidgetResizable(True)
+        mainLayout.addWidget(scrollArea)
+        self.scrollWidget = QtWidgets.QWidget()
+        scrollArea.setWidget(self.scrollWidget)
+        self.gridLayout = QtWidgets.QGridLayout(self.scrollWidget)
 
-        button_size = 70
-        self.shape_folder = os.path.join(spath, 'icons')
-        self.shape_files = [f for f in os.listdir(self.shape_folder) if f.endswith('.png')]
-        self.button_icon_map = {}
+        buttonSize = 70
+        self.shapeFolder = os.path.join(spath, 'icons')
+        self.shapeFiles = [f for f in os.listdir(self.shapeFolder) if f.endswith('.png')]
+        self.buttonIconMap = {}
 
-        for i, shape_file in enumerate(self.shape_files):
+        for i, shapeFile in enumerate(self.shapeFiles):
             button = QtWidgets.QPushButton()
-            button.setFixedSize(button_size, button_size)
-            icon_path = os.path.join(self.shape_folder, shape_file)
-            button.setIcon(QtGui.QIcon(icon_path))
-            button.setIconSize(QtCore.QSize(button_size, button_size))
+            button.setFixedSize(buttonSize, buttonSize)
+            iconPath = os.path.join(self.shapeFolder, shapeFile)
+            button.setIcon(QtGui.QIcon(iconPath))
+            button.setIconSize(QtCore.QSize(buttonSize, buttonSize))
             row, col = i // 5, i % 5
-            self.grid_layout.addWidget(button, row, col)
-            self.button_icon_map[button] = shape_file
-            button.clicked.connect(self.button_clicked)
+            self.gridLayout.addWidget(button, row, col)
+            self.buttonIconMap[button] = shapeFile
+            button.clicked.connect(self.buttonClicked)
 
-        self.change_shape_radio.toggled.connect(self.toggle_combo_lock)
+        self.changeShapeRadio.toggled.connect(self.toggleComboLock)
 
-    def toggle_combo_lock(self, checked):
+    def toggleComboLock(self, checked):
         """Toggle the combo box based on the radio button state."""
-        self.range_combo.setEnabled(not checked)
+        self.rangeCombo.setEnabled(not checked)
 
-    def button_clicked(self):
+    def buttonClicked(self):
         """Handle button clicks to create or change controllers."""
         cmds.undoInfo(openChunk=True)
         sender = self.sender()
-        selected_range = self.range_combo.currentText()
+        selectedRange = self.rangeCombo.currentText()
 
-        if self.create_radio.isChecked():
+        if self.createRadio.isChecked():
             sel = cmds.ls(sl=True)
             if not sel:
-                self.create_curve()
+                self.createCurve()
             else:
-                if selected_range == 'Only Ctrl':
-                    self.create_only_ctrl(sel)
-                elif selected_range == 'Constraint':
-                    self.create_constraint(sel)
-                elif selected_range == 'Joint Based':
-                    self.create_joint_based(sel)
-                elif selected_range == 'Connection':
-                    self.create_connection(sel)
+                if selectedRange == 'Only Ctrl':
+                    self.createOnlyCtrl(sel)
+                elif selectedRange == 'Constraint':
+                    self.createConstraint(sel)
+                elif selectedRange == 'Joint Based':
+                    self.createJointBased(sel)
+                elif selectedRange == 'Connection':
+                    self.createConnection(sel)
 
-        if self.change_shape_radio.isChecked():
-            self.change_curve()
+        if self.changeShapeRadio.isChecked():
+            self.changeCurve()
 
         cmds.undoInfo(closeChunk=True)
 
-    def create_only_ctrl(self, selection):
+    def createOnlyCtrl(self, selection):
         """Create controllers and parent them to selected objects."""
         for obj in selection:
-            ctrl = self.create_curve()
+            ctrl = self.createCurve()
             cmds.parentConstraint(obj, ctrl, mo=False)
             cmds.delete(ctrl, cn=True)
 
-    def create_constraint(self, selection):
+    def createConstraint(self, selection):
         """Create controllers with constraints to selected objects."""
         for obj in selection:
             trans = cmds.createNode("transform", name=obj + "_ctrl")
             transOff = cmds.createNode("transform", name=trans + "_off")
             cmds.parent(trans, transOff)
-            ctrl = self.create_curve()
+            ctrl = self.createCurve()
             ccshape = cmds.listRelatives(ctrl, shapes=True)
             cmds.delete(cmds.parentConstraint(obj, transOff, mo=False))
             cmds.parentConstraint(trans, obj, mo=True)
@@ -106,14 +107,14 @@ class CreateControllerUI(QtWidgets.QDialog):
             cmds.parent(ccshape, trans, relative=True, shape=True)
             cmds.delete(ctrl)
 
-    def create_joint_based(self, selection):
+    def createJointBased(self, selection):
         """Create joint-based controllers for selected objects."""
         for obj in selection:
             trans = cmds.createNode("transform", name=obj + "_ctrl")
             transOff = cmds.createNode("transform", name=trans + "_off")
             cmds.parent(trans, transOff)
             jnt = cmds.createNode("joint", name=obj + "_jj")
-            ctrl = self.create_curve()
+            ctrl = self.createCurve()
             ctrlShape = cmds.listRelatives(ctrl, shapes=True)
             cmds.delete(cmds.parentConstraint(obj, transOff, mo=False))
             cmds.parentConstraint(trans, jnt, mo=False)
@@ -122,7 +123,7 @@ class CreateControllerUI(QtWidgets.QDialog):
             cmds.delete(ctrl)
             cmds.skinCluster(jnt, obj)
 
-    def create_connection(self, selection):
+    def createConnection(self, selection):
         """Create controllers with connections to selected objects' attributes."""
         axies = "XYZ"
         attrs = ["translate", "rotate", "scale"]
@@ -137,33 +138,36 @@ class CreateControllerUI(QtWidgets.QDialog):
             for attr in attrs:
                 for axis in axies:
                     cmds.connectAttr(trans + "." + attr + axis, obj + "." + attr + axis)
-            ctrl = self.create_curve()
+            ctrl = self.createCurve()
             ctrlShape = cmds.listRelatives(ctrl, shapes=True)
             cmds.parent(ctrlShape, trans, relative=True, shape=True)
             cmds.delete(ctrl)
 
-    def create_curve(self):
+    def createCurve(self):
         """Create a curve based on the selected button's icon."""
         sender = self.sender()
-        clicked_icon = self.button_icon_map.get(sender)
+        clickedIcon = self.buttonIconMap.get(sender)
 
-        if clicked_icon:
-            shapeName = clicked_icon.replace('.png', '.mel')
+        if clickedIcon:
+            shapeName = clickedIcon.replace('.png', '.mel')
             shapePath = os.path.join(f'{spath}/shapes/', shapeName)
             name = shapeName.split(".")[0]
             if os.path.exists(shapePath):
                 trans = cmds.createNode('transform', name=name)
                 mel.eval(f'source "{shapePath}";')
+                shape = cmds.listRelatives(trans, s=True)[0]
+                print(shape)
+                cmds.rename(shape, f'{name}Shape')
                 return trans
             else:
                 print(f"Shape file not found: {shapePath}")
         return None
 
-    def change_curve(self):
+    def changeCurve(self):
         """Replace selected controllers with new curves."""
         oldCtrls = cmds.ls(sl=True)
         for oldCtrl in oldCtrls:
-            newCtrl = self.create_curve()
+            newCtrl = self.createCurve()
             if newCtrl:
                 cmds.delete(cmds.parentConstraint(oldCtrl, newCtrl))
                 cmds.parent(newCtrl, oldCtrl)
@@ -182,6 +186,7 @@ class CreateControllerUI(QtWidgets.QDialog):
                 cmds.delete(newCtrl)
                 cmds.delete(oldShapes)
         cmds.select(clear=True)
+
 
 def launchUI():
     """Run the UI."""
